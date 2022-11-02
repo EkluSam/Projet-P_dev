f734f11463a91aace8f6c37447e2258c3c87c20f
Sam <pn88doo@eduvaud.ch>
2022-11-02 16:11:13 +0100

    Multiple updates
    Updated Menu.cs and RocketShip.cs

--- SpicyInvader/SpicyInvader/Menu.cs
+++ SpicyInvader/SpicyInvader/Menu.cs
@@ -15,67 +15,67 @@ namespace SpicyInvader
 
         #region Menu
         private const string _TITLE = @"
-         _____ _____        _____ ______   _____ _   ___      __     _____  ______ _____   _____ 
-        / ____|  __ \ /\   / ____|  ____| |_   _| \ | \ \    / /\   |  __ \|  ____|  __ \ / ____|
-       | (___ | |__) /  \ | |    | |__      | | |  \| |\ \  / /  \  | |  | | |__  | |__) | (___  
-        \___ \|  ___/ /\ \| |    |  __|     | | | . ` | \ \/ / /\ \ | |  | |  __| |  _  / \___ \ 
-        ____) | |  / ____ \ |____| |____   _| |_| |\  |  \  / ____ \| |__| | |____| | \ \ ____) |
-       |_____/|_| /_/    \_\_____|______| |_____|_| \_|   \/_/    \_\_____/|______|_|  \_\_____/ 
+                 _____ _____        _____ ______   _____ _   ___      __     _____  ______ _____   _____ 
+                / ____|  __ \ /\   / ____|  ____| |_   _| \ | \ \    / /\   |  __ \|  ____|  __ \ / ____|
+               | (___ | |__) /  \ | |    | |__      | | |  \| |\ \  / /  \  | |  | | |__  | |__) | (___  
+                \___ \|  ___/ /\ \| |    |  __|     | | | . ` | \ \/ / /\ \ | |  | |  __| |  _  / \___ \ 
+                ____) | |  / ____ \ |____| |____   _| |_| |\  |  \  / ____ \| |__| | |____| | \ \ ____) |
+               |_____/|_| /_/    \_\_____|______| |_____|_| \_|   \/_/    \_\_____/|______|_|  \_\_____/ 
                                                                                            
         ";
         private const string _PLAY = @"
-                                     ______  __  _________ 
-                                 __ / / __ \/ / / / __/ _ \
-                                / // / /_/ / /_/ / _// , _/
-                                \___/\____/\____/___/_/|_|             
+                                                     ______  __  _________ 
+                                                 __ / / __ \/ / / / __/ _ \
+                                                / // / /_/ / /_/ / _// , _/
+                                                \___/\____/\____/___/_/|_|             
         ";
 
         private const string _OPTION = @"
-                              ____  ___  ______________  _  __
-                             / __ \/ _ \/_  __/  _/ __ \/ |/ / 
-                            / /_/ / ___/ / / _/ // /_/ /    / 
-                            \____/_/    /_/ /___/\____/_/|_/ 
+                                              ____  ___  ______________  _  __
+                                             / __ \/ _ \/_  __/  _/ __ \/ |/ / 
+                                            / /_/ / ___/ / / _/ // /_/ /    / 
+                                            \____/_/    /_/ /___/\____/_/|_/ 
         ";
 
         private const string _HIGHSCORE = @"
-                           __ _____________ _______________  ___  ____
-                          / // /  _/ ___/ // / __/ ___/ __ \/ _ \/ __/
-                         / _  // // (_ / _  /\ \/ /__/ /_/ / , _/ _/  
-                        /_//_/___/\___/_//_/___/\___/\____/_/|_/___/     
+                                       __ _____________ _______________  ___  ____
+                                      / // /  _/ ___/ // / __/ ___/ __ \/ _ \/ __/
+                                     / _  // // (_ / _  /\ \/ /__/ /_/ / , _/ _/  
+                                    /_//_/___/\___/_//_/___/\___/\____/_/|_/___/     
         ";
 
         private const string _ABOUT = @"
-                           ___     ___  ___  ____  ___  ____  ____
-                          / _ |   / _ \/ _ \/ __ \/ _ \/ __ \/ __/
-                         / __ |  / ___/ , _/ /_/ / ___/ /_/ /\ \  
-                        /_/ |_| /_/  /_/|_|\____/_/   \____/___/                                          
+                                          ___     ___  ___  ____  ___  ____  ____
+                                         / _ |   / _ \/ _ \/ __ \/ _ \/ __ \/ __/
+                                        / __ |  / ___/ , _/ /_/ / ___/ /_/ /\ \  
+                                       /_/ |_| /_/  /_/|_|\____/_/   \____/___/                                          
         ";
 
         private const string _QUIT = @"
-                          ____  __  _________________________ 
-                         / __ \/ / / /  _/_  __/_  __/ __/ _ \
-                        / /_/ / /_/ // /  / /   / / / _// , _/
-                        \___\_\____/___/ /_/   /_/ /___/_/|_|
+                                          ____  __  _________________________ 
+                                         / __ \/ / / /  _/_  __/_  __/ __/ _ \
+                                        / /_/ / /_/ // /  / /   / / / _// , _/
+                                        \___\_\____/___/ /_/   /_/ /___/_/|_|
         ";
 
         private const string _DIFFICULTY = @"
-                                     ___ _______          ____   __
-                                 ___/ (_) _/ _(_)_____ __/ / /__/_/
-                                / _  / / _/ _/ / __/ // / / __/ -_)
-                                \_,_/_/_//_//_/\__/\_,_/_/\__/\__/
+                                                 ___ _______          ____   __
+                                             ___/ (_) _/ _(_)_____ __/ / /__/_/
+                                            / _  / / _/ _/ / __/ // / / __/ -_)
+                                            \_,_/_/_//_//_/\__/\_,_/_/\__/\__/
         ";
 
         private const string _SOUND = @"
-                                           ____             
-                                          / __/__  ___      
-                                         _\ \/ _ \/ _ \     
-                                        /___/\___/_//_/  
+                                                       ____             
+                                                      / __/__  ___      
+                                                     _\ \/ _ \/ _ \     
+                                                    /___/\___/_//_/  
         ";
         private const string _NAVARROW = "  <--- ";
         #endregion
 
         public int yPos = 12; // position de base
-        public int xPos = 75; // position de base
+        public int xPos = 85; // position de base
         /// <summary>
         /// Méthode qui permet la navigation dans le menu
         /// yPos = 12 -> JOUER
@@ -184,7 +184,7 @@ namespace SpicyInvader
         {
             Console.SetWindowSize(120, 50);
             Console.BufferHeight = 50;
-
+            
             Title();
 
             Console.ForegroundColor = ConsoleColor.White;
@@ -225,23 +225,92 @@ namespace SpicyInvader
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(_DIFFICULTY);
             Console.WriteLine(_SOUND);
-
+            if(ArrowMovesOptions() == 1)
+            {
+                // Menu difficulté
+            }
+            else
+            {
+                // Menu son
+            }
             
         }
 
         public short ArrowMovesOptions()
         {
             short i = 0;
-            int xPos = 35;
-            int yPos = 8;
+            int xPos = 80;
+            int yPos = 9;
 
             Console.SetCursorPosition(xPos, yPos);
             Console.ForegroundColor = ConsoleColor.Red;
             Console.Write(_NAVARROW);
 
+            while (true)
+            {
+                ConsoleKeyInfo Key = Console.ReadKey(true);
+                if (Key.Key == ConsoleKey.UpArrow)
+                {
+                    if (yPos <= 9)
+                    {
+                        Console.SetCursorPosition(xPos, yPos);
+                        Console.Write("         ");
+                        yPos = 15;
+                        Console.SetCursorPosition(xPos, yPos);
+                        Console.Write(_NAVARROW);
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(xPos, yPos);
+                        Console.Write("         ");
+                        yPos -= 6;
+                        Console.SetCursorPosition(xPos, yPos);
+                        Console.Write(_NAVARROW);
+                    }
+                }
 
-            return i;
-                
+                if (Key.Key == ConsoleKey.DownArrow)
+                {
+                    if (yPos >= 15)
+                    {
+                        Console.SetCursorPosition(xPos, yPos);
+                        Console.Write("         ");
+                        yPos = 9;
+                        Console.SetCursorPosition(xPos, yPos);
+                        Console.Write(_NAVARROW);
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(xPos, yPos);
+                        Console.Write("         ");
+                        yPos += 6;
+                        Console.SetCursorPosition(xPos, yPos);
+                        Console.Write(_NAVARROW);
+                    }
+                }
+
+                switch (yPos)
+                {
+                    case 9:
+                        if (Key.Key == ConsoleKey.Enter)
+                        {
+                            i = 1;
+                            return i;
+                        }
+                        if(Key.Key == ConsoleKey.Escape)
+                        {
+                            Console.Clear();
+                        }
+                        break;
+                    case 15:
+                        if (Key.Key == ConsoleKey.Enter)
+                        {
+                            i = 2;
+                            return i;
+                        }
+                        break;
+                }
+            }
         }
     }
 }
--- SpicyInvader/SpicyInvader/RocketShip.cs
+++ SpicyInvader/SpicyInvader/RocketShip.cs
@@ -24,6 +24,21 @@ namespace SpicyInvader
             set { _y = value; }
         }
 
+        private const string _SHIP = @"
+                 ▲
+                ███
+              ███████
+            ███████████";
 
+        public string Ship
+        {
+            get { return _SHIP; }
+        }
+
+        public RocketShip(int X, int Y)
+        {
+            _x = X;
+            _y = Y;
+        }
     }
 }
